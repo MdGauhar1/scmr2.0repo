376ef57e1e862d799f1c7145dded7a2dd7a3e220
gauhar <mdgauhar0007@gmail.com>
2024-12-24 13:25:37 +0530

    fourth-commit

--- src/main/java/com/scm/controllers/PageController.java
+++ src/main/java/com/scm/controllers/PageController.java
@@ -1,11 +1,23 @@
 package com.scm.controllers;
 
+import com.scm.entities.User;
+import com.scm.forms.UserForm;
+import com.scm.helpers.Message;
+import com.scm.helpers.MessageType;
+import com.scm.services.UserService;
+import jakarta.servlet.http.HttpSession;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
 public class PageController {
+
+    @Autowired
+    private UserService userService;
 
     @RequestMapping("/home")
     public String home(Model model) {
@@ -44,12 +56,41 @@ public class PageController {
         return "login";
     }
 
+// showing register page
     @RequestMapping("/register")
     public String registerPage(Model model) {
-        System.out.println("this is service");
-        model.addAttribute("register");
+        UserForm userForm = new UserForm();
+        model.addAttribute("userForm", userForm);
         return "register";
     }
+//processing register
 
+    @RequestMapping(value ="/do-register", method=RequestMethod.POST)
+    public String processRegister(@ModelAttribute UserForm userForm,HttpSession session) {
+
+        //validate
+        //save ti database
+        User user = new User();
+        user.setName(userForm.getName());
+        user.setEmail(userForm.getEmail());
+        user.setPassword(userForm.getPassword());
+        user.setAbout(userForm.getAbout());
+        user.setPhoneNumber(userForm.getPhoneNumber());
+        user.setEnabled(false);
+        user.setProfilePic(
+                "https://www.learncodewithdurgesh.com/_next/image?url=%2F_next%2Fstatic%2Fmedia%2Fdurgesh_sir.35c6cb78.webp&w=1920&q=75");
+
+        User savedUser = userService.saveUser(user);
+
+        System.out.println("user saved :");
+
+        //add message for sucessfull or faliur
+        // Message message = Message.builder().content("Registration Successful").type(MessageType.green).build();
+        Message message = new Message("Registration Successful", MessageType.green);
+        session.setAttribute("message",message);
+
+        //redirect
+        return "redirect:/register";
+    }
 
 }
--- src/main/java/com/scm/entities/User.java
+++ src/main/java/com/scm/entities/User.java
@@ -1,8 +1,10 @@
 package com.scm.entities;
 
 
+
 import jakarta.persistence.*;
 import lombok.*;
+
 
 import java.lang.reflect.Type;
 import java.util.ArrayList;
@@ -15,6 +17,7 @@ import java.util.List;
 @AllArgsConstructor
 @NoArgsConstructor
 @Builder
+
 public class User {
 
     @Id
@@ -33,9 +36,10 @@ public class User {
     //INFORMATION
     private boolean enabled = false;
     private boolean emailVerified=false;
-    private boolean passwordVerified=false;
+    private boolean phoneVerified = false;
 
     //for the providers
+    @Enumerated(EnumType.STRING)
     private Providers provider = Providers.SELF;
     private String providerUserId;
 
@@ -44,5 +48,118 @@ public class User {
                                                                // also fetch will only fetch  when needed
     private List<Contact> contacts = new ArrayList<>();
 
+
+
+
+
+
+
+
+
+
+
+    public String getUserId() {
+        return userId;
+    }
+
+    public void setUserId(String userId) {
+        this.userId = userId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getAbout() {
+        return about;
+    }
+
+    public void setAbout(String about) {
+        this.about = about;
+    }
+
+    public String getProfilePic() {
+        return profilePic;
+    }
+
+    public void setProfilePic(String profilePic) {
+        this.profilePic = profilePic;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public boolean isEnabled() {
+        return enabled;
+    }
+
+    public void setEnabled(boolean enabled) {
+        this.enabled = enabled;
+    }
+
+    public boolean isEmailVerified() {
+        return emailVerified;
+    }
+
+    public void setEmailVerified(boolean emailVerified) {
+        this.emailVerified = emailVerified;
+    }
+
+    public boolean isPhoneVerified() {
+        return phoneVerified;
+    }
+
+    public void setPhoneVerified(boolean passwordVerified) {
+        this.phoneVerified = passwordVerified;
+    }
+
+    public Providers getProvider() {
+        return provider;
+    }
+
+    public void setProvider(Providers provider) {
+        this.provider = provider;
+    }
+
+    public String getProviderUserId() {
+        return providerUserId;
+    }
+
+    public void setProviderUserId(String providerUserId) {
+        this.providerUserId = providerUserId;
+    }
+
+    public List<Contact> getContacts() {
+        return contacts;
+    }
+
+    public void setContacts(List<Contact> contacts) {
+        this.contacts = contacts;
+    }
 
 }
